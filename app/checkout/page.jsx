a0a43e8ba34b296722b3c8e6e2b384414713b35b
Alaa-Eldeyn <alaa.eldeyn@gmail.com>
2024-02-13 19:10:26 +0200

    Upload Files

---
+++ app/checkout/page.jsx
@@ -0,0 +1,26 @@
+"use client";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import CheckoutForm from "./_components/CheckoutForm";
+import { useSearchParams } from "next/navigation";
+
+// Make sure to call `loadStripe` outside of a component’s render to avoid
+// recreating the `Stripe` object on every render.
+const stripePromise = loadStripe(process.env.NEXT_PUBLIC_Publishable_key);
+
+export default function App() {
+  const searchParams = useSearchParams();
+  const options = {
+    // passing the client secret obtained from the server
+    mode: "payment",
+    currency: "usd",
+    amount: Number(searchParams.get("amount"))*100,
+    // clientSecret: "{{CLIENT_SECRET}}",
+  };
+
+  return (
+    <Elements stripe={stripePromise} options={options}>
+      <CheckoutForm amount={Number(searchParams.get("amount"))} />
+    </Elements>
+  );
+}
