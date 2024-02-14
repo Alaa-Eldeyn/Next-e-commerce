03541e702d0946db697d65591a0b6d7bf58136e9
Alaa-Eldeyn <alaa.eldeyn@gmail.com>
2024-02-14 21:47:13 +0200

    .

--- app/api/send-email/route.jsx
+++ app/api/send-email/route.jsx
@@ -1,10 +1,10 @@
-import { EmailTemplate } from "../../_components/email-template";
+import { EmailTemplate } from "../../_components/email-template.tsx";
 import { Resend } from "resend";
 
-const resend = new Resend("re_V4cmaZGf_DknxXDLaCpWWi6S2GAyAkMsv");
+const resend = new Resend(process.env.RESEND_API_KEY);
+
 export async function POST() {
   try {
-    console.log(resend);
     const data = await resend.emails.send({
       from: "onboarding@resend.dev",
       to: ["alislami474@gmail.com"],
