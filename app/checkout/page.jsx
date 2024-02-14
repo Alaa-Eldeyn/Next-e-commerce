43e4202d588692a670b5d8d2139d2b5229b817d5
Alaa-Eldeyn <alaa.eldeyn@gmail.com>
2024-02-14 22:11:51 +0200

    .

--- app/checkout/page.jsx
+++ app/checkout/page.jsx
@@ -3,27 +3,24 @@ import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import CheckoutForm from "./_components/CheckoutForm";
 import { useSearchParams } from "next/navigation";
-import { Suspense } from "react";
 
 // Make sure to call `loadStripe` outside of a component’s render to avoid
 // recreating the `Stripe` object on every render.
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_Publishable_key);
 
-export default function App() {
+function Checkout() {
   const searchParams = useSearchParams();
   const options = {
-    // passing the client secret obtained from the server
     mode: "payment",
     currency: "usd",
-    amount: Number(searchParams.get("amount")) * 100,
-    // clientSecret: "{{CLIENT_SECRET}}",
+    amount: searchParams.get("amount") * 100,
   };
-
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <Elements stripe={stripePromise} options={options}>
-        <CheckoutForm amount={Number(searchParams.get("amount"))} />
-      </Elements>
-    </Suspense>
+    <Elements stripe={stripePromise} options={options}>
+      <CheckoutForm amount={Number(searchParams.get("amount"))} />
+    </Elements>
   );
 }
+
+
+export default Checkout;
